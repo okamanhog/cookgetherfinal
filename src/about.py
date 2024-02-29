2f349a7a54f6b324f8cdfca2c6da554dabcf4317
okamanhog <149702779+okamanhog@users.noreply.github.com>
2024-02-29 18:15:26 +0100

    add features and instructions to our aboutme.py page

--- src/about.py
+++ src/about.py
@@ -1,8 +1,20 @@
 import streamlit as st
 
+
+# Reference: We used ChatGPT to make our About Page better readable.
 def about_page():
-    st.title("About")
-    st.markdown("**Cookgether is an app for building cooking community and recipes generating.**")
-    st.markdown("In this app, we provide you with the AI recipes generator where you can generate your personalised, recipes based on your cooking conditions, such as the ingredients you have, the type of cuisine you want to cook, the cooking appliances in your kitchen, the number of people you are cooking for, the time you have, to generate your personalised recipes. You can also download them.")
-    st.markdown("You can also share your recipes in the community")
-    st.markdown("There is also food share and money calculator function")
+    st.title("About Cookgether")
+    st.markdown("""
+    **Cookgether** is a cutting-edge platform designed for cooking enthusiasts. It fosters a vibrant community where members can generate AI-powered personalized recipes, share their culinary creations and engage with others over shared food interests. 
+
+    ### Features Include:
+    - **AI Recipe Generator**: Tailor-made recipes based on your input such as available ingredients, preferred cuisine, cooking appliances, and more.
+    - **Community Sharing**: A space to share your recipes with the community, rate, and review others' contributions.
+    - **Food Share**: An initiative to share leftover food, promoting sustainability and community support.
+    - **User Profile**: Personalized profiles to showcase your cooking adventures, favorite recipes, and culinary preferences.
+
+    ### How to Get Started:
+    - **Online Access**: Visit our public link at [https://cookgether-project.streamlit.app/](https://cookgether-project.streamlit.app/) to start your Cookgether journey.
+    - **Local Setup**: Clone or download the app, set up your environment with Streamlit and other dependencies, and run the app locally for a more personalized experience.
+
+    Built by [Sum](https://github.com/SumSumarie) and [Lennart](https://github.com/okamanhog).""")
